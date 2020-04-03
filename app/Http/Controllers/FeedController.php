cb35fd69288c17e60ed63e72713801881bb61a55
Corey A <corey@agstrategic.design>
2020-04-03 12:30:32 -0400

    front end improvements

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -28,180 +28,180 @@ class FeedController extends Controller
         $selects = $this->generateDateSelectBox(); 
 
 
-         
-        return view('testshome', compact('total_tests_data_today', 'number_of_new_tests', 'selects'));
-    }
-
-    public function calculateNewTests($today_number_of_tests){
-
-        $total_tests_previous_day = $this->previousDaysTests();
-
-        return $result = $today_number_of_tests - $total_tests_previous_day; 
-
-    }
-
-
-    public function previousDaysTests(){
-
-        $previous_day_data = $this->getDailyTestsUS(); 
-
-        return $total_tests_previous_day = $previous_day_data["totalTestResults"]; 
-
-
-
-    }
-
-    public function getDailyTestsUS(){
-
-        $query = "USDAILY"; 
-
-        $all_day_data = $this->queryApi($query); 
-
-        return $previous_day_data = $all_day_data[0]; 
-
-
         
+        return view('testshome', compact('total_tests_data_today', 'number_of_new_tests', 'selects'));
+}
 
-    }
+public function calculateNewTests($today_number_of_tests){
+
+    $total_tests_previous_day = $this->previousDaysTests();
+
+    return $result = $today_number_of_tests - $total_tests_previous_day; 
+
+}
 
 
-    public function generateDateSelectBox(){
+public function previousDaysTests(){
 
-         $selects = []; 
+    $previous_day_data = $this->getDailyTestsUS(); 
+
+    return $total_tests_previous_day = $previous_day_data["totalTestResults"]; 
+
+
+
+}
+
+public function getDailyTestsUS(){
+
+    $query = "USDAILY"; 
+
+    $all_day_data = $this->queryApi($query); 
+
+    return $previous_day_data = $all_day_data[0]; 
+
+
+    
+
+}
+
+
+public function generateDateSelectBox(){
+
+   $selects = []; 
 
 
         // $today = Carbon::today();
 
-         $today = Carbon::now();
+   $today = Carbon::now();
 
 
-        $dt = new Carbon($today, 'America/New_York');
+   $dt = new Carbon($today, 'America/New_York');
 
-        $select_array = [];
+   $select_array = [];
 
-        
+   
 
-        for ($i = 1; $i <= 15;$i++){
+   for ($i = 1; $i <= 15;$i++){
 
-            $dt = new Carbon($today, 'America/New_York');
-            $previous_day = $dt->subDay($i); 
-            $date_string = strtotime($previous_day); 
+    $dt = new Carbon($today, 'America/New_York');
+    $previous_day = $dt->subDay($i); 
+    $date_string = strtotime($previous_day); 
 
-            $formatted_date_value = date('Ymd', $date_string);
+    $formatted_date_value = date('Ymd', $date_string);
 
-        $date_word = date("F j, Y", $date_string); 
+    $date_word = date("F j, Y", $date_string); 
 
-        $select_array = [$formatted_date_value, $date_word]; 
+    $select_array = [$formatted_date_value, $date_word]; 
 
-        $selects[] = $select_array; 
-
-
-        }
-          
-
-        return $selects; 
- 
-
-    }
-
-    public function formatDate($date){
-
-         $datestring = strtotime($date);
-
-         return date("F j, Y", $datestring);
-    }
-
-    public function comparePage(){
+    $selects[] = $select_array; 
 
 
-    		$this->validate(request(), [
+}
+
+
+return $selects; 
+
+
+}
+
+public function formatDate($date){
+
+   $datestring = strtotime($date);
+
+   return date("F j, Y", $datestring);
+}
+
+public function comparePage(){
+
+
+  $this->validate(request(), [
 			//edit these to coressponding user fields
-			'state' => 'required', 
-			'datepicker' => 'required'  
-			
-			
+     'state' => 'required', 
+     'datepicker' => 'required'  
+     
+     
 
-		]);
+ ]);
 
-    		$feed = new Feed(); 
+$feed = new Feed(); 
 
-    		$feed->original_day = $this->parseDate(request('datepicker')); 
-
-        
+$feed->original_day = $this->parseDate(request('datepicker')); 
 
 
-            $feed->original_day_formatted = $this->formatDate($feed->original_day); 
 
-    		$feed->previous_day = $this->parsePreviousDate($feed->original_day);
 
-            $feed->previous_day_formatted = $this->formatDate($feed->previous_day); 
+$feed->original_day_formatted = $this->formatDate($feed->original_day); 
 
-    		$feed->state = request('state'); 
+$feed->previous_day = $this->parsePreviousDate($feed->original_day);
 
-    		$feed->state_query = $this->generateStateQuery($feed->state); 
+$feed->previous_day_formatted = $this->formatDate($feed->previous_day); 
+
+$feed->state = request('state'); 
+
+$feed->state_query = $this->generateStateQuery($feed->state); 
 
     		//compile the query string
-    		$feed->query1 = $this->compileQuery($feed->state_query, $feed->original_day); 
+$feed->query1 = $this->compileQuery($feed->state_query, $feed->original_day); 
 
-    		$feed->query2 = $this->compileQuery($feed->state_query, $feed->previous_day); 
+$feed->query2 = $this->compileQuery($feed->state_query, $feed->previous_day); 
 
     	//run the queries to generate page
-    	$feed->page_data_day1 = $this->queryApi($feed->query1); 
-    	$feed->page_data_day2 = $this->queryApi($feed->query2); 
+$feed->page_data_day1 = $this->queryApi($feed->query1); 
+$feed->page_data_day2 = $this->queryApi($feed->query2); 
     	// dd($feed); 
-    		return view('layouts.days',compact('feed')); 
+return view('layouts.days',compact('feed')); 
 
-    }
+}
 
-     public function compilePageData(){
+public function compilePageData(){
 
-    	$date1_from_form = '2020-03-31 11:08 AM';
-    	$date2_from_form = '2020-03-30 11:08 AM';
+   $date1_from_form = '2020-03-31 11:08 AM';
+   $date2_from_form = '2020-03-30 11:08 AM';
 
-    	$state_from_form = 'PA'; 
+   $state_from_form = 'PA'; 
 
-    	$day1 = $this->parseDate($date1_from_form); 
-    	$day2 = $this->parseDate($date2_from_form); 
+   $day1 = $this->parseDate($date1_from_form); 
+   $day2 = $this->parseDate($date2_from_form); 
 
-    	$state = $state_from_form; 
+   $state = $state_from_form; 
 
-    	$state_query = $this->generateStateQuery($state); 
+   $state_query = $this->generateStateQuery($state); 
 
     	//compile the query string
-    	$query1 = $this->compileQuery($state_query, $day1); 
+   $query1 = $this->compileQuery($state_query, $day1); 
 
-    	$query2 = $this->compileQuery($state_query, $day2); 
+   $query2 = $this->compileQuery($state_query, $day2); 
 
     	//run the queries to generate page
-    	$page_data_day1 = $this->queryApi($query1); 
-    	$page_data_day2 = $this->queryApi($query2); 
- 
+   $page_data_day1 = $this->queryApi($query1); 
+   $page_data_day2 = $this->queryApi($query2); 
+   
 
-    }
+}
 
 
-    public function generateStateQuery($state){
+public function generateStateQuery($state){
 
     		//day and state comes from form submission
-    	
-    	return $query = "states/daily?state=".$state; 
-    		
-    
+   
+   return $query = "states/daily?state=".$state; 
+   
+   
 
-    }
+}
 
 
-    public function parseDate($date_from_form){
+public function parseDate($date_from_form){
 
   //  
 	$datestring = strtotime($date_from_form); 
 	
 	return $datestring = date('Ymd', $datestring);
 
-	 
-    }
+  
+}
 
-     public function parsePreviousDate($date_from_form){
+public function parsePreviousDate($date_from_form){
 
   //  
 	$datestring = strtotime($date_from_form); 
@@ -216,62 +216,62 @@ class FeedController extends Controller
 
 	// $timelogged_timestamp = Carbon::parse($date_from_form, 'UTC');
 
-		 
-    }
+ 
+}
 
 
-    public function compileQuery($statequery, $day){
+public function compileQuery($statequery, $day){
 
-    	return $query = $statequery."&date=".$day;
+   return $query = $statequery."&date=".$day;
 
-    }
+}
 
-   
-    public function queryAPI($query){
-		$client = new Client();
 
-        $base_url = env('BASE_URL');
-        
-        
-        $curlstring = $base_url; 
-        if($query == 'US'){
-		
+public function queryAPI($query){
+  $client = new Client();
 
-		$curlstring.= 'us'; 
+  $base_url = env('BASE_URL');
+  
+  
+  $curlstring = $base_url; 
+  if($query == 'US'){
+      
 
-        }
+      $curlstring.= 'us'; 
 
-        elseif ($query == 'USDAILY') {
-            $curlstring.= 'us/daily'; 
-        }
+  }
 
-        else{
-            $base_url = env('BASE_URL');
-        
-        
-        $curlstring = $base_url; 
+  elseif ($query == 'USDAILY') {
+    $curlstring.= 'us/daily'; 
+}
 
-        $curlstring.= $query; 
-
-        }
-
-		$response = $client->get($curlstring);
+else{
+    $base_url = env('BASE_URL');
     
-		if($response->getStatusCode() == 200){
+    
+    $curlstring = $base_url; 
 
-		$data = $response->getBody()->getContents();
-		
-		$result = json_decode($data, true);
+    $curlstring.= $query; 
 
-		}
-		else 
-			$result = null; 
+}
 
-		return $result; 
+$response = $client->get($curlstring);
 
-       
-		
+if($response->getStatusCode() == 200){
 
-	}
+  $data = $response->getBody()->getContents();
+  
+  $result = json_decode($data, true);
+
+}
+else 
+ $result = null; 
+
+return $result; 
+
+
+
+
+}
 
 }
--- resources/views/testshome.blade.php
+++ resources/views/testshome.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="container">
 
-    
 
-        <div class="row justify-content-center">
+
+    <div class="row justify-content-center">
         <div class="col">
 
-            <h1 class="text-center">Tests Today</h1>
+            <h1 class="text-center">United States COVID Tests Today</h1>
             <p class="text-center"><small >{{$total_tests_data_today[0]["lastModified"]}}</small></p>
 
-            <h2 class="text-center">{{$number_of_new_tests}} new tests today</h2>
+            <h2 class="text-center">{{number_format($number_of_new_tests)}} new tests today</h2>
 
-            <h2 class="text-center">{{$total_tests_data_today[0]["totalTestResults"]}} total</h2>
+            <h2 class="text-center">{{number_format($total_tests_data_today[0]["totalTestResults"])}} total</h2>
 
             
             
@@ -24,118 +24,112 @@
             <p>This site would not be possible without the data collected by the <a href="https://covidtracking.com" target="_blank">Covid Tracking project</a>. Data from the site is gathered using their <a href="https://covidtracking.com/api" target="_blank">API</a>.</p>
 
         </div>
-        </div>
+    </div>
 
 
     <div class="row justify-content-center">
         <div class="col">
 
-            
+
 
             <h2>Test Comparison by State</h2>
             <form method="POST" action="/compare">
 
 
-{{-- include csrf field in all of our forms for authentication --}}
-     {{ csrf_field() }}
+                {{-- include csrf field in all of our forms for authentication --}}
+                {{ csrf_field() }}
 
-     <div class="form-group">
+                <div class="form-group">
 
-                <fieldset>
-                    
+                    <fieldset>
 
-                    <div>
-                        
 
-                        {{-- Have to convert from string to unix using strtotime and then to SQL time --}}
+                        <div>
+
+
+                            {{-- Have to convert from string to unix using strtotime and then to SQL time --}}
                       {{--   <input type="datepicker" id="datepicker" name="datepicker"
 
                         min="2020-03-04" max="2200-12-31"   
                         required /> --}}
 
-                          <legend>Select date</legend>
-            <label>Compare 
-            <select name="datepicker">
-@foreach($selects as $select)
-    <option value="{{$select[0]}}">{{$select[1]}}</option>
-@endforeach
-</select>
-            to previous day</label>          
-                    </div>
-                       <!-- <c-datepicker></c-datepicker> -->
-                    
+                        <legend>Select comparison</legend>
+                        <fieldset>
+                            <label>Compare 
+                                <select name="datepicker">
+                                    @foreach($selects as $select)
+                                    <option value="{{$select[0]}}">{{$select[1]}}</option>
+                                    @endforeach
+                                </select>
+                                to previous day's data in      
 
-                </fieldset>
-            </div>
-            <legend>State</legend>
-            <label>Select the state for comparison: </label>
-            <select name="state">
-    <option value="AL">Alabama</option>
-    <option value="AK">Alaska</option>
-    <option value="AZ">Arizona</option>
-    <option value="AR">Arkansas</option>
-    <option value="CA">California</option>
-    <option value="CO">Colorado</option>
-    <option value="CT">Connecticut</option>
-    <option value="DE">Delaware</option>
-    <option value="DC">District Of Columbia</option>
-    <option value="FL">Florida</option>
-    <option value="GA">Georgia</option>
-    <option value="HI">Hawaii</option>
-    <option value="ID">Idaho</option>
-    <option value="IL">Illinois</option>
-    <option value="IN">Indiana</option>
-    <option value="IA">Iowa</option>
-    <option value="KS">Kansas</option>
-    <option value="KY">Kentucky</option>
-    <option value="LA">Louisiana</option>
-    <option value="ME">Maine</option>
-    <option value="MD">Maryland</option>
-    <option value="MA">Massachusetts</option>
-    <option value="MI">Michigan</option>
-    <option value="MN">Minnesota</option>
-    <option value="MS">Mississippi</option>
-    <option value="MO">Missouri</option>
-    <option value="MT">Montana</option>
-    <option value="NE">Nebraska</option>
-    <option value="NV">Nevada</option>
-    <option value="NH">New Hampshire</option>
-    <option value="NJ">New Jersey</option>
-    <option value="NM">New Mexico</option>
-    <option value="NY">New York</option>
-    <option value="NC">North Carolina</option>
-    <option value="ND">North Dakota</option>
-    <option value="OH">Ohio</option>
-    <option value="OK">Oklahoma</option>
-    <option value="OR">Oregon</option>
-    <option value="PA">Pennsylvania</option>
-    <option value="RI">Rhode Island</option>
-    <option value="SC">South Carolina</option>
-    <option value="SD">South Dakota</option>
-    <option value="TN">Tennessee</option>
-    <option value="TX">Texas</option>
-    <option value="UT">Utah</option>
-    <option value="VT">Vermont</option>
-    <option value="VA">Virginia</option>
-    <option value="WA">Washington</option>
-    <option value="WV">West Virginia</option>
-    <option value="WI">Wisconsin</option>
-    <option value="WY">Wyoming</option>
-</select>  
+                            <select name="state">
+                                <option value="AL">Alabama</option>
+                                <option value="AK">Alaska</option>
+                                <option value="AZ">Arizona</option>
+                                <option value="AR">Arkansas</option>
+                                <option value="CA">California</option>
+                                <option value="CO">Colorado</option>
+                                <option value="CT">Connecticut</option>
+                                <option value="DE">Delaware</option>
+                                <option value="DC">District Of Columbia</option>
+                                <option value="FL">Florida</option>
+                                <option value="GA">Georgia</option>
+                                <option value="HI">Hawaii</option>
+                                <option value="ID">Idaho</option>
+                                <option value="IL">Illinois</option>
+                                <option value="IN">Indiana</option>
+                                <option value="IA">Iowa</option>
+                                <option value="KS">Kansas</option>
+                                <option value="KY">Kentucky</option>
+                                <option value="LA">Louisiana</option>
+                                <option value="ME">Maine</option>
+                                <option value="MD">Maryland</option>
+                                <option value="MA">Massachusetts</option>
+                                <option value="MI">Michigan</option>
+                                <option value="MN">Minnesota</option>
+                                <option value="MS">Mississippi</option>
+                                <option value="MO">Missouri</option>
+                                <option value="MT">Montana</option>
+                                <option value="NE">Nebraska</option>
+                                <option value="NV">Nevada</option>
+                                <option value="NH">New Hampshire</option>
+                                <option value="NJ">New Jersey</option>
+                                <option value="NM">New Mexico</option>
+                                <option value="NY">New York</option>
+                                <option value="NC">North Carolina</option>
+                                <option value="ND">North Dakota</option>
+                                <option value="OH">Ohio</option>
+                                <option value="OK">Oklahoma</option>
+                                <option value="OR">Oregon</option>
+                                <option selected="selected" value="PA">Pennsylvania</option>
+                                <option value="RI">Rhode Island</option>
+                                <option value="SC">South Carolina</option>
+                                <option value="SD">South Dakota</option>
+                                <option value="TN">Tennessee</option>
+                                <option value="TX">Texas</option>
+                                <option value="UT">Utah</option>
+                                <option value="VT">Vermont</option>
+                                <option value="VA">Virginia</option>
+                                <option value="WA">Washington</option>
+                                <option value="WV">West Virginia</option>
+                                <option value="WI">Wisconsin</option>
+                                <option value="WY">Wyoming</option>
+                            </select>  
+                        </label></fieldset></div>
 
+                        <div class="form-group">
+                            <p>
+                              <button type="submit" class="btn btn-primary">Submit</button>
+                          </p>
+                      </div> 
 
-<div class="form-group">
-<p>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</p>
-</div> 
+                  </form>
 
-</form>
-
-        </div>
-    </div>
-</div>
+              </div>
+          </div>
+      </div>
 
 
 
-@endsection
+      @endsection
