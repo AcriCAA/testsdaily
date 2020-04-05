bc7135fd6f028c0946c729ea9d5bb015778f6899
Corey A <corey@agstrategic.design>
2020-04-05 12:31:12 -0400

    front end improvements

--- resources/views/layouts/days.blade.php
+++ resources/views/layouts/days.blade.php
@@ -2,120 +2,89 @@
 
 @section('content')
 
-	<h2 class="text-center">{{$feed->state}}</h2>
-	<h5 class="text-center">{{$feed->original_day_formatted}}</h5>
-  <p class="text-center"><small >updated as of: {{$feed->page_data_day1["dateChecked"]}}</small></p>
-		<h3 class="text-center">Increased Number of Tests from Previous Day: {{number_format($feed->page_data_day1["totalTestResultsIncrease"])}}</h3>
-      
-   <h4 class="text-center">hospitalized increase: 
-      	{{number_format($feed->page_data_day1["hospitalizedIncrease"])}}</h4>
+	
 
-        <!-- eg https://covidtracking.com/api/states/daily?state=NY&date=20200316 -->
-      <p class="text-center"><small><a href="https://covidtracking.com/api/states/daily?state={{$feed->state}}&date={{$feed->original_day}}">source</a></small></p>
-
-	<div class="card-group">
+   <div class="card-group">
   <div class="card">
-    
-    <div class="card-body">
-      <h3 class="card-title">{{$feed->original_day_formatted}}</h3>
-      
-      
-
-
-    
-    
    
-
-    @if(!empty($feed->page_data_day1["positive"]))
-      <p class="card-text">postive: {{number_format($feed->page_data_day1["positive"])}}</p>
-      <hr class="my-1">
-    @endif
-
-    @if(!empty($feed->page_data_day1["negative"]))
-      <p class="card-text">negative: {{number_format($feed->page_data_day1["negative"])}}</p>
-      <hr class="my-1">
-    @endif
-     
-    @if(!empty($feed->page_data_day1["hospitalized"]))
-      <p class="card-text">hospitalized: {{number_format($feed->page_data_day1["hospitalized"])}}</p>
-      <hr class="my-1">
-    @endif
-
-    @if(!empty($feed->page_data_day1["hospitalizedIncrease"]))
-    <p class="card-text">hospitalized increase: 
-      	{{number_format($feed->page_data_day1["hospitalizedIncrease"])}}</p>
-        <hr class="my-1">
-    @endif
-
-      @if(!empty($feed->page_data_day1["negativeIncrease"]))
-      	<p class="card-text">negative increase: {{number_format($feed->page_data_day1["negativeIncrease"])}}</p>
-        <hr class="my-1">
-      @endif
-
-
-      @if(!empty($feed->page_data_day1["positiveIncrease"]))
-      	<p class="card-text">positive increase: {{number_format($feed->page_data_day1["positiveIncrease"])}}</p>
-        <hr class="my-1">
-      @endif
-
-       @if(!empty($feed->page_data_day1["total"]))
-     <p class="card-text">cumulative tests completed since testing began: {{number_format($feed->page_data_day1["total"])}}</p>
-      
-    @endif
-        
-      	
-    </div>
-  </div>
-  <div class="card">
-    
     <div class="card-body">
-      <h3 class="card-title">{{$feed->previous_day_formatted}}</h3>
-      
-      
 
-     
+      <h2 class="text-center display-4">{{$feed->state}}</h2>
+  <h5 class="text-center">{{$feed->original_day_formatted}}</h5>
+  <p class="text-center"><small >updated as of: {{$feed->page_data_day1["dateChecked"]}}</small></p>
 
-      @if(!empty($feed->page_data_day2["positive"]))
-      <p class="card-text">postive: {{number_format($feed->page_data_day2["positive"])}}</p>
-      <hr class="my-1">
-      @endif
 
-      @if(!empty($feed->page_data_day2["negative"]))
-      <p class="card-text">negative: {{number_format($feed->page_data_day2["negative"])}}</p>
-      <hr class="my-1">
-      @endif
+  @if(!empty(number_format($feed->page_data_day1["totalTestResultsIncrease"])))
+		<h3 class="text-center">Increased Number of Tests from Previous Day</h3>
 
-    @if(!empty($feed->page_data_day2["hospitalizedIncrease"]))
-      <p class="card-text">hospitalized: {{number_format($feed->page_data_day2["hospitalized"])}}</p>
-      <hr class="my-1">
+    <p class="lead text-center">{{number_format($feed->page_data_day1["totalTestResultsIncrease"])}}</p>
+
+    {{-- <hr class="my-1"> --}}
+
     @endif
+</div></div></div>
 
-    @if(!empty($feed->page_data_day2["hospitalizedIncrease"]))
-      <p class="card-text">hospitalized increase: 
-      	{{number_format($feed->page_data_day2["hospitalizedIncrease"])}}</p>
-        <hr class="my-1">
-      @endif
 
-    @if(!empty($feed->page_data_day2["negativeIncrease"]))
-      	<p class="card-text">negative increase: {{number_format($feed->page_data_day2["negativeIncrease"])}}</p>
-        <hr class="my-1">
-    @endif
 
-    @if(!empty($feed->page_data_day2["positiveIncrease"]))
-      	<p class="card-text">positive increase: {{number_format($feed->page_data_day2["positiveIncrease"])}}</p>
-        <hr class="my-1">
-    @endif
-
-     @if(!empty($feed->page_data_day2["total"]))
-      <p class="card-text">cumulative tests since testing began: {{number_format($feed->page_data_day2["total"])}}</p>
       
-      @endif
 
 
-      	
+
+    <div class="card-group">
+  <div class="card">
+   
+    <div class="card-body">
+          @if(!empty($feed->page_data_day1["hospitalizedIncrease"]))
+      
+   <h4 class="text-center">hospitalized increase 
+        </h4>                                                    
+
+           <p class="text-center lead">
+        {{number_format($feed->page_data_day1["hospitalizedIncrease"])}}</p>
+
+
+
+    @endif
+    
     </div>
   </div>
   
+  <div class="card">
+   
+    <div class="card-body">
+
+       @if(!empty($feed->page_data_day1["hospitalizedIncrease"]))
+      
+   <h4 class="text-center">death increase 
+        </h4>                                                    
+
+           <p class="text-center lead">
+        {{number_format($feed->page_data_day1["deathIncrease"])}}</p>
+
+
+
+    @endif
+      
+    </div>
+  </div>
 </div>
 
+{{--         deathIncrease --}}
+
+        <!-- eg https://covidtracking.com/api/states/daily?state=NY&date=20200316 -->
+    
+
+
+
+@include('layouts.table')
+<div class="container">
+  
+  <div class="row">
+    <div class="col">
+       <p class="text-center"><small><a href="https://covidtracking.com/api/states/daily?state={{$feed->state}}&date={{$feed->original_day}}">source data</a></small></p>
+    </div>
+  </div>
+</div>
+ 
 @endsection
+
---
+++ resources/views/layouts/table.blade.php
@@ -0,0 +1,210 @@
+<div class="container">
+  <div class="row mt-3">
+    <div class="col border">
+
+      <h2 class="text-center p-3">Days Comparison</h2  >
+      <table class="table table-striped">
+        <thead>
+          <tr>
+
+            <th scope="col"class="text-right">{{$feed->original_day_formatted}}</th>
+            <th scope="col" class="text-center"></th>
+            <th scope="col" class="text-left">{{$feed->previous_day_formatted}}</th>
+          </tr>
+        </thead>
+        <tbody>
+    {{-- <tr>
+      
+      <td class="text-right">
+         @if(!empty($feed->page_data_day1["positive"]))
+        {{number_format($feed->page_data_day1["positive"])}}
+       @endif
+      </td>
+
+       <td class="text-center">positive</td>
+
+        <td class="text-left">
+         @if(!empty($feed->page_data_day2["positive"]))
+        {{number_format($feed->page_data_day2["positive"])}}
+       @endif
+      </td>
+
+      
+    </tr> --}}
+
+    {{-- <tr>
+      
+      <td class="text-right">
+
+      @if(!empty($feed->page_data_day1["negative"]))
+      {{number_format($feed->page_data_day1["negative"])}}
+      
+        @endif
+
+      </td>
+
+     <td class="text-center">
+      negative
+    </td>
+
+      <td class="text-left"> 
+
+        @if(!empty($feed->page_data_day2["negative"]))
+      {{number_format($feed->page_data_day2["negative"])}}
+    
+        @endif</td>
+      
+      
+      </tr> --}}
+
+
+
+      <tr>
+
+        <td class="text-right">
+
+         @if(!empty($feed->page_data_day1["hospitalizedIncrease"]))
+
+         {{number_format($feed->page_data_day1["hospitalizedIncrease"])}}
+
+         @endif
+
+       </td>
+
+       <td class="text-center">
+        hospitalized increase
+      </td>
+
+      <td class="text-left">
+
+       @if(!empty($feed->page_data_day2["hospitalizedIncrease"]))
+
+       {{number_format($feed->page_data_day2["hospitalizedIncrease"])}}
+       
+       @endif
+
+     </td>
+
+   </tr>
+
+   <tr>
+
+    <td class="text-right">
+      @if(!empty($feed->page_data_day1["negativeIncrease"]))
+      {{number_format($feed->page_data_day1["negativeIncrease"])}}
+
+      @endif        
+    </td>
+
+    <td class="text-center">
+      negative increase
+    </td>
+
+    <td class="text-left">
+     @if(!empty($feed->page_data_day2["negativeIncrease"]))
+     {{number_format($feed->page_data_day2["negativeIncrease"])}}
+
+     @endif
+
+   </td>
+
+ </tr>
+
+ <tr>
+
+  <td class="text-right">
+   @if(!empty($feed->page_data_day1["positiveIncrease"]))
+   {{number_format($feed->page_data_day1["positiveIncrease"])}}
+
+   @endif              
+ </td>
+
+ <td class="text-center">
+  positive increase
+</td>
+
+<td class="text-left">
+  @if(!empty($feed->page_data_day2["positiveIncrease"]))
+  {{number_format($feed->page_data_day2["positiveIncrease"])}}
+
+  @endif  
+
+</td>
+
+</tr>
+
+
+<tr>
+
+  <td class="text-right">
+
+    @if(!empty($feed->page_data_day1["hospitalized"]))
+    {{number_format($feed->page_data_day1["hospitalized"])}}
+
+    @endif
+
+  </td>
+
+  <td class="text-center">
+    hospitalized cumulative
+  </td>
+
+  <td class="text-left">
+
+    @if(!empty($feed->page_data_day2["hospitalized"]))
+    {{number_format($feed->page_data_day2["hospitalized"])}}
+
+    @endif
+
+  </td>
+
+
+
+</tr>
+<tr>
+
+  <td class="text-right">
+   @if(!empty($feed->page_data_day1["total"]))
+   {{number_format($feed->page_data_day1["total"])}}
+
+   @endif
+
+ </td>
+
+ <td class="text-center">
+  cumulative tests completed since testing began
+</td>
+
+<td class="text-left">
+ @if(!empty($feed->page_data_day2["total"]))
+ {{number_format($feed->page_data_day2["total"])}}
+
+ @endif
+
+</td>
+
+</tr>
+
+ {{--           <tr>
+      
+      <td class="text-right">
+              
+      </td>
+
+      <td class="text-center">
+        
+      </td>
+
+      <td class="text-left">
+        
+
+      </td>
+
+    </tr> --}}
+
+
+  </tbody>
+</table>
+</div>
+</div>
+</div
