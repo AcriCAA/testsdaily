200090d6b4c63d2ae693b109ce278588911359af
Corey Acri <acri.corey@gmail.com>
2020-04-07 09:41:04 -0400

    arrows

--- resources/views/layouts/table.blade.php
+++ resources/views/layouts/table.blade.php
@@ -2,11 +2,10 @@
   <div class="row mt-3">
     <div class="col border">
 
-      <h2 class="text-center p-3">Days Comparison</h2  >
+      <h2 class="text-center p-3">Days Comparison</h2>
       <table class="table table-striped">
         <thead>
           <tr>
-
             <th scope="col"class="text-right">{{$feed->original_day_formatted}}</th>
             <th scope="col" class="text-center"></th>
             <th scope="col" class="text-left">{{$feed->previous_day_formatted}}</th>
@@ -25,8 +24,15 @@
 
          {{number_format($feed->page_data_day1["hospitalizedIncrease"])}}
 
+        @if($feed->page_data_day1["hospitalizedIncrease"] > $feed->page_data_day2["hospitalizedIncrease"])
+         <i class="fas fa-chevron-circle-up text-danger"></i>
+         @elseif($feed->page_data_day1["hospitalizedIncrease"] < $feed->page_data_day2["hospitalizedIncrease"])
+        <i class="fas fa-chevron-circle-down text-success"></i>
          @endif
 
+         @endif
+
+        
        </td>
 
        <td class="text-center">
@@ -51,7 +57,19 @@
       @if(!empty($feed->page_data_day1["negativeIncrease"]))
       {{number_format($feed->page_data_day1["negativeIncrease"])}}
 
-      @endif        
+
+       @if($feed->page_data_day1["negativeIncrease"] > $feed->page_data_day2["negativeIncrease"])
+         <i class="fas fa-chevron-circle-up text-danger"></i>
+         @elseif($feed->page_data_day1["negativeIncrease"] < $feed->page_data_day2["negativeIncrease"])
+        <i class="fas fa-chevron-circle-down text-success"></i>
+         @endif     
+
+
+      @endif   
+
+
+
+
     </td>
 
     <td class="text-center">
@@ -73,6 +91,13 @@
   <td class="text-right">
    @if(!empty($feed->page_data_day1["positiveIncrease"]))
    {{number_format($feed->page_data_day1["positiveIncrease"])}}
+
+    @if($feed->page_data_day1["positiveIncrease"] > $feed->page_data_day2["positiveIncrease"])
+         <i class="fas fa-chevron-circle-up text-danger"></i>
+         @elseif($feed->page_data_day1["positiveIncrease"] < $feed->page_data_day2["positiveIncrease"])
+        <i class="fas fa-chevron-circle-down text-success"></i>
+         @endif     
+
 
    @endif              
  </td>
@@ -118,7 +143,75 @@
 
 
 
+</tr>]
+
+
+<tr>
+
+  <td class="text-right">
+   @if(!empty($feed->page_data_day1["positive"]))
+   {{number_format($feed->page_data_day1["positive"])}}
+
+   @if($feed->page_data_day1["positive"] > $feed->page_data_day2["positive"])
+         <i class="fas fa-chevron-circle-up text-danger"></i>
+         @elseif($feed->page_data_day1["positive"] < $feed->page_data_day2["positive"])
+        <i class="fas fa-chevron-circle-down text-success"></i>
+         @endif     
+
+
+   @endif
+
+ </td>
+
+ <td class="text-center">
+  cumulative positives since testing began
+</td>
+
+<td class="text-left">
+ @if(!empty($feed->page_data_day2["positive"]))
+ {{number_format($feed->page_data_day2["positive"])}}
+
+ @endif
+
+</td>
+
 </tr>
+
+
+<tr>
+
+  <td class="text-right">
+   @if(!empty($feed->page_data_day1["negative"]))
+   {{number_format($feed->page_data_day1["negative"])}}
+
+   @if($feed->page_data_day1["negative"] > $feed->page_data_day2["negative"])
+         <i class="fas fa-chevron-circle-up text-danger"></i>
+         @elseif($feed->page_data_day1["negative"] < $feed->page_data_day2["negative"])
+        <i class="fas fa-chevron-circle-down text-success"></i>
+         @endif     
+
+
+   @endif
+
+ </td>
+
+ <td class="text-center">
+  cumulative negatives since testing began
+</td>
+
+<td class="text-left">
+ @if(!empty($feed->page_data_day2["positive"]))
+ {{number_format($feed->page_data_day2["positive"])}}
+
+ @endif
+
+</td>
+
+</tr>
+
+
+
+
 <tr>
 
   <td class="text-right">
